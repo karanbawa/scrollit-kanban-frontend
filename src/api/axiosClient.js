ba8f0eafb10c280780a85c8614c2e1bbebe6ac49
Karan Bawa <karan.bawa@nagarro.com>
2023-01-26 21:35:54 +0530

    code push

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseUrl = "https://scrollit-kanban-backend.vercel.app/app/v1/";
+const baseUrl = "https://scrollit-kanban-backend.vercel.app/api/v1/";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
